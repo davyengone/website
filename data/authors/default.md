70c1feb1b194f02b74fcc3f4f53cfd91a1a1898f
davyengone <davy@hackages.io>
2022-02-06 00:20:05 +0100

    The 100X Projects

--- data/authors/default.md
+++ data/authors/default.md
@@ -1,16 +1,15 @@
 ---
-name: Tails Azimuth
-avatar: /static/images/avatar.png
-occupation: Professor of Atmospheric Science
-company: Stanford University
-email: address@yoursite.com
-twitter: https://twitter.com/Twitter
+name: Davy Engone
+avatar: /static/images/davyengone.png
+occupation: Founder and Growth Engineer
+company: Hackages Ventures
+email: idea@davyengone.io
+twitter: https://twitter.com/davyengone
 linkedin: https://www.linkedin.com
-github: https://github.com
+github: https://github.com/davyengone
 ---
 
-Tails Azimuth is a professor of atmospheric sciences at the Stanford AI Lab. His research interests includes complexity modelling of tailwinds, headwinds and crosswinds.
+Davy is a software engineer, an educator, a runner, a father and Marjolein's best partner.
 
-He leads the clean energy group which develops 3D air pollution-climate models, writes differential equation solvers, and manufactures titanium plated air ballons. In his free time he bakes raspberry pi.
-
-Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed neque elit, tristique placerat feugiat ac, facilisis vitae arcu. Proin eget egestas augue. Praesent ut sem nec arcu pellentesque aliquet. Duis dapibus diam vel metus tempus vulputate.
+Davy has built companies like Sporten and Philos and Hackages. Throught his works, he has
+impacted thousand of engineers.
--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -1,25 +1,24 @@
 const siteMetadata = {
-  title: 'Next.js Starter Blog',
-  author: 'Tails Azimuth',
-  headerTitle: 'TailwindBlog',
-  description: 'A blog created with Next.js and Tailwind.css',
+  title: 'From ideas to product with Davy Engone',
+  author: 'Davy Engone',
+  headerTitle: 'Davy Engone',
+  description: '',
   language: 'en-us',
   theme: 'system', // system, dark or light
-  siteUrl: 'https://tailwind-nextjs-starter-blog.vercel.app',
-  siteRepo: 'https://github.com/timlrx/tailwind-nextjs-starter-blog',
-  siteLogo: '/static/images/logo.png',
-  image: '/static/images/avatar.png',
+  siteUrl: 'https://davyengone.io',
+  siteRepo: 'https://github.com/davyengone/website.git',
+  siteLogo: '/static/images/davyengone.png',
+  image: '/static/images/davyengone.png',
   socialBanner: '/static/images/twitter-card.png',
-  email: 'address@yoursite.com',
-  github: 'https://github.com',
-  twitter: 'https://twitter.com/Twitter',
-  facebook: 'https://facebook.com',
-  youtube: 'https://youtube.com',
-  linkedin: 'https://www.linkedin.com',
+  email: 'ideas@davyengone.io',
+  github: 'https://github.com/davyengone',
+  twitter: 'https://twitter.com/davyengone',
+  youtube: 'https://www.youtube.com/channel/UCKmzC1m6b-riW_H7VC0Xu4A',
+  linkedin: 'https://www.linkedin.com/in/davyengone/',
   locale: 'en-US',
   analytics: {
     // supports plausible, simpleAnalytics, umami or googleAnalytics
-    plausibleDataDomain: '', // e.g. tailwind-nextjs-starter-blog.vercel.app
+    plausibleDataDomain: 'davyengone.io', // e.g. tailwind-nextjs-starter-blog.vercel.app
     simpleAnalytics: false, // true or false
     umamiWebsiteId: '', // e.g. 123e4567-e89b-12d3-a456-426614174000
     googleAnalyticsId: '', // e.g. UA-000000-2 or G-XXXXXXX
